3ae5674767ba64a73a51f52543682c136ad04154
Jan1si <nikita.evgrafov@inbox.ru>
2022-09-09 14:28:06 +0500

    Вывод данных в карточку

--- src/App.js
+++ src/App.js
@@ -2,6 +2,28 @@ import { Card } from "./components/Card";
 import { Drawer } from "./components/Drawer";
 import { Header } from "./components/Header";
 
+const arr = [{
+  title: "Мужские Кроссовки Nike Blazer Mid Suede",
+  price: 12999,
+  img: "1.jpg",
+},
+{
+  title: "Мужские Кроссовки Nike Air Max 270",
+  price: 13999,
+  img: "2.jpg",
+},
+{
+  title: "Мужские Кроссовки Nike Blazer Mid Suede",
+  price: 8499,
+  img: "3.jpg",
+},
+{
+  title: "Кроссовки Puma X Aka Boku Future Rider",
+  price: 8999,
+  img: "4.jpg",
+}
+];
+
 function App() {
   return (
     <div className="wrapper">
@@ -16,10 +38,14 @@ function App() {
           </div>
         </div>
         <ul className="list__product mt-40 d-flex">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {arr.map((item, key) =>
+            <Card
+              key={key}
+              titleProduct={item.title}
+              priceProduct={item.price}
+              imgProduct={item.img}
+            />
+          )}
         </ul>
       </div>
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
-export const Card = () => {
+export const Card = (props) => {
   return (
     <div className="card ">
       <div className="favorite">
         <img src="img/unliked.svg" alt="Unliked" />
       </div>
-      <img width={133} height={112} className="card__img" src="img/sneakers/2.jpg" alt="" />
-      <p className="card__name mt-15">Мужские Кроссовки Nike Blazer Mid Suede</p>
+      <img width={133} height={112} className="card__img" src={`img/sneakers/${props.imgProduct}`} alt="" />
+      <p className="card__name mt-15">{props.titleProduct}</p>
       <div className="card__info d-flex justify-between mt-15 align-center">
         <div className="card__price d-flex flex-column">
           <span>Цена: </span>
-          <b>12000 р.</b>
+          <b>{props.priceProduct} р.</b>
         </div>
         <button className="btn__cart">
           <img width={11} height={11} src="img/plus.svg" alt="Plus" />
