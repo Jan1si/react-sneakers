418d39c0edc9fbf9146a054024fc6e0b800929a6
Jan1si <nikita.evgrafov@inbox.ru>
2022-10-06 21:37:03 +0500

    zxc

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,27 +1,29 @@
 import React from 'react'
 import styles from './Card.module.scss';
 import { useState } from 'react';
+import { LoadSpiner } from '../LoadSpiner'
 
 
-export const Card = ({ id, title, price, imgUrl, onPlus, onFavorite, inFavorite }) => {
+export const Card = ({title, price, imgUrl, onPlus, onFavorite }) => {
 
   const [isAdded, setIsAdded] = useState(false);
   const [isFavorite, setIsFavorite] = useState(false);
+  const [loadPost, setLoadPost] = useState(false);
 
   const handleClick = () => {
-    onPlus({ id, title, price, imgUrl });
+    onPlus({title, price, imgUrl}, setLoadPost);
     setIsAdded(!isAdded);
   }
 
   const clickFavorite = () => {
-    onFavorite({ id, title, price, imgUrl });
+    onFavorite({title, price, imgUrl});
     setIsFavorite(!isFavorite);
   }
 
   return (
     <div className={styles.card}>
       <div className={styles.favorite} onClick={clickFavorite}>
-        <img src={isFavorite || inFavorite ?  "/img/liked.svg" : "/img/unliked.svg"} alt="Unliked" />
+        <img src={`${isFavorite ? "/img/liked.svg" :"/img/unliked.svg"}`} alt="Unliked" />
       </div>
       <img width={133} height={112} className={styles.card__img} src={`img/sneakers/${imgUrl}`} alt="" />
       <p className={`${styles.card__name} mt-15`}>{title}</p>
@@ -30,10 +32,13 @@ export const Card = ({ id, title, price, imgUrl, onPlus, onFavorite, inFavorite 
           <span>Цена: </span>
           <b>{price} р.</b>
         </div>
-        <img
-          className={styles.plus}
-          onClick={handleClick}
-          src={isAdded ? "/img/btn-checked.svg" : "/img/btn-plus.svg"} alt="Plus" />
+        {loadPost ? <LoadSpiner /> :
+          (<img
+            className={styles.plus}
+            onClick={handleClick}
+            src={isAdded ? "/img/btn-checked.svg" : "/img/btn-plus.svg"} alt="Plus" />
+          )
+        }
       </div>
     </div>
   )
