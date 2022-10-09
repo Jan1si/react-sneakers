ae60db16c0ce8dfe4b2a2a4887d36663a5dcd3f0
Jan1si <nikita.evgrafov@inbox.ru>
2022-10-10 03:31:01 +0500

    Merge branch 'main' of https://github.com/Jan1si/react-sneakers

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,29 +4,28 @@ import { AppContext } from '../../App';
 import { useContext } from 'react';
 
 
-export const Card = ({ id, title, price, imgUrl, onPlus, onFavorite, inFavorite }) => {
+export const Card = (
+  { id,
+    title,
+    price,
+    imgUrl,
+    onPlus,
+    onFavorite, }) => {
 
-  const [isAdded, setIsAdded] = useState(false);
-  const [isFavorite, setIsFavorite] = useState(false);
+  const { hasAddToCart, hasAddToFavorite } = useContext(AppContext);
 
   const handleClick = () => {
     onPlus({ id, title, price, imgUrl });
-    setIsAdded(!isAdded);
   }
 
   const clickFavorite = () => {
     onFavorite({ id, title, price, imgUrl });
-    setIsFavorite(!isFavorite);
   }
 
   return (
     <div className={styles.card}>
       <div className={styles.favorite} onClick={clickFavorite}>
-<<<<<<< HEAD
-        <img src={`${isFavorite ? "/img/liked.svg" :"/img/unliked.svg"}`} alt="Unliked" />
-=======
         <img src={hasAddToFavorite(id) ? "/img/liked.svg" : "/img/unliked.svg"} alt="Unliked" />
->>>>>>> 82654b21c9943168c6a6db63a242ef3e3914a0db
       </div>
       <img width={133} height={112} className={styles.card__img} src={`img/sneakers/${imgUrl}`} alt="" />
       <p className={`${styles.card__name} mt-15`}>{title}</p>
@@ -35,21 +34,12 @@ export const Card = ({ id, title, price, imgUrl, onPlus, onFavorite, inFavorite 
           <span>Цена: </span>
           <b>{price} р.</b>
         </div>
-<<<<<<< HEAD
-        {loadPost ? <LoadSpiner /> :
-          (<img
-            className={styles.plus}
-            onClick={handleClick}
-            src={isAdded ? "/img/btn-checked.svg" : "/img/btn-plus.svg"} alt="Plus" />
-          )
-        }
-=======
         <img
           className={styles.plus}
           onClick={handleClick}
           src={hasAddToCart(id) ? "/img/btn-checked.svg" : "/img/btn-plus.svg"} alt="Plus" />
->>>>>>> 82654b21c9943168c6a6db63a242ef3e3914a0db
       </div>
+
     </div>
   )
 }
